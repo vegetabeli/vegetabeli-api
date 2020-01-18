9845ee7df8345aca6d6edde127799fe30e51c7b0
Satrio Utomo <szatrio@gmail.com>
2020-01-18 17:03:17 +0700

    minor update

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ const logger = require ('morgan')
 const cors = require ('cors')
 const helmet = require ('helmet')
 const bodyParser = require ('body-parser')
+const redis = require ('redis')
 
+const client = redis.createClient(process.env.REDIS_PORT)
 
 const router = require('./src/routes/index')
 
 const index = express ();
 
-index.listen (5000, () => {
-    console.log ('Server is running on port 5000')
+index.listen (process.env.PORT || 5000, () => {
+    console.log ('Server is running on port' + process.env.PORT)
 })
 
 index.use (logger('dev'))
@@ -19,6 +21,19 @@ index.use (helmet.xssFilter())
 index.use (cors())
 index.use (bodyParser.json())
 index.use (bodyParser.urlencoded({extended: false}))
+
+const cache = ( req, res, next) => {
+    const username = req.params.username
+
+    client.get(username, (err, data) =>{
+        if(err) throw err
+        if(data !== null){
+            res.send(setResponse(username, data))
+        }else{
+            next()
+        }
+    })
+}
 
 index.use('/', router)
 
--- src/models/cart.js
+++ src/models/cart.js
@@ -26,7 +26,8 @@ module.exports = {
             LEFT JOIN product ON product.id_product = product_taken.id_product
             JOIN user ON user.id_user = cart.id_user
             JOIN market ON market.id_market = cart.id_market
-            GROUP BY cart.id_cart WHERE id_cart='${id}'`, (err, response) =>{
+            WHERE cart.id_cart='${id}'
+            GROUP BY cart.id_cart`, (err, response) =>{
                 if (!err) {
                     resolve (response)
                 }else{
